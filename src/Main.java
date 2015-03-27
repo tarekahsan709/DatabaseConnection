5c4b9f94c872c5f9b01b793bcc5ad35eb7ca3a1e
Muhammad.Nazmul Ahsan <tarekahsan709@gmail.com>
2015-03-28 02:52:18 +0600

    New syntex for java 7

--- src/Main.java
+++ src/Main.java
@@ -7,44 +7,25 @@ import java.sql.Statement;
 
 public class Main
 {		
-	
+
 	public static void main(String[] args) throws SQLException
 	{ 
-		Connection con=null;
-		Statement stm=null;
-		ResultSet resultSet=null;
-		
-		try
-		{
-			con=DatabaseConnection.getConnection(DbName.hSqlDb);
-			System.out.println("Connection successfull");
-			stm=con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
-			resultSet=stm.executeQuery("SELECT * FROM STATES");
-			resultSet.last();
-			System.out.println("Our current row number is "+resultSet.getRow());
-			
-		} catch (SQLException e) 
-		{
-			System.out.println("Here is the error"+e);
-		}finally
-		{
-			if(resultSet!=null)
-			{
-				resultSet.close();
-			}
-			if(stm!=null)
-			{
-				stm.close();
-			}
-			if(con!=null)
-			{
-				con.close();
-			}
-			
-		}
-	
-	
-		
+
+
+		try(Connection con=DatabaseConnection.getConnection(DbName.hSqlDb);
+				Statement stm=con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+				ResultSet resultSet=stm.executeQuery("SELECT * FROM STATES");
+				)
+				{
+			        System.out.println("Our current row number is "+resultSet.getRow());
+
+				} catch (SQLException e) 
+				{
+					System.out.println("Here is the error"+e);
+				}
+
+
+
 
 	}
 
